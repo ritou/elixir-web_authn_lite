914c95076abe9d03ae75bf736966a943eb233d9b
ritou <ritou.06@gmail.com>
2018-11-05 16:07:05 +0900

    fix returned format and add validate func

--- lib/web_authn_lite/client_data_json.ex
+++ lib/web_authn_lite/client_data_json.ex
@@ -18,15 +18,30 @@ defmodule WebAuthnLite.ClientDataJSON do
     try do
       with raw <- base64_url_encoded_client_data_json |> Base.url_decode64!(padding: false),
            json <- raw |> Jason.decode!() do
-        %__MODULE__{
+        {:ok, %__MODULE__{
           type: json["type"],
           origin: json["origin"],
           challenge: json["challenge"],
           raw: raw
-        }
+        }}
       end
     rescue
       _ -> {:error, :invalid_format}
+    end
+  end
+
+  @spec validate(base64_url_encoded_client_data_json :: String.t(), type :: String.t, origin :: String.t, challenge :: String.t) ::
+          {:ok, t} | {:error, :invalid_format} | {:error, :invalid_type} | {:error, :invalid_origin} | {:error, :invalid_challenge}
+  def validate(base64_url_encoded_client_data_json, type, origin, challenge) do
+    with {:ok, client_data} <- decode(base64_url_encoded_client_data_json) do
+      cond do
+        client_data.type != type -> {:error, :invalid_type}
+        client_data.origin != origin -> {:error, :invalid_origin}
+        client_data.challenge != challenge -> {:error, :invalid_challenge}
+        true -> {:ok, client_data}
+      end
+    else
+      error -> error
     end
   end
 end
--- test/lib/web_authn_lite/client_data_json_text.exs
+++ test/lib/web_authn_lite/client_data_json_text.exs
@@ -11,7 +11,7 @@ defmodule WebAuthnLite.ClientDataJSONTest do
   @valid_type "webauthn.create"
 
   test "decode" do
-    assert client_data = ClientDataJSON.decode(@valid_encoded_client_data_json)
+    assert {:ok, client_data} = ClientDataJSON.decode(@valid_encoded_client_data_json)
     assert client_data.challenge == @valid_challenge
     assert client_data.origin == @valid_origin
     assert client_data.type == @valid_type
@@ -19,4 +19,13 @@ defmodule WebAuthnLite.ClientDataJSONTest do
 
     assert {:error, :invalid_format} == ClientDataJSON.decode(@invalid_encoded_client_data_json)
   end
+
+  test "validate" do
+    assert {:ok, _} = ClientDataJSON.validate(@valid_encoded_client_data_json, @valid_type, @valid_origin, @valid_challenge)
+    assert {:error, :invalid_format} == ClientDataJSON.validate(@invalid_encoded_client_data_json, @valid_type, @valid_origin, @valid_challenge)
+    assert {:error, :invalid_type} == ClientDataJSON.validate(@valid_encoded_client_data_json, "invalid", @valid_origin, @valid_challenge)
+    assert {:error, :invalid_origin} == ClientDataJSON.validate(@valid_encoded_client_data_json, @valid_type, "invalid", @valid_challenge)
+    assert {:error, :invalid_challenge} == ClientDataJSON.validate(@valid_encoded_client_data_json, @valid_type, @valid_origin, "invalid")
+  end
+
 end
