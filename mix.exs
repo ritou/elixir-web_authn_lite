c4bb442530192cc7bf8941fb608ba67e7760a2ab
ritou <ritou.06@gmail.com>
2018-11-08 01:15:29 +0900

    update supported elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WebAuthnLite.MixProject do
     [
       app: :web_authn_lite,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
