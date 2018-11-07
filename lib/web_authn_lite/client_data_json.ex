9ca72171d7d86e0e9e8e965b2ef3886a53fe588c
ritou <ritou.06@gmail.com>
2018-11-08 02:04:03 +0900

    run formatter to ClientDataJSON

--- lib/web_authn_lite/client_data_json.ex
+++ lib/web_authn_lite/client_data_json.ex
@@ -21,12 +21,13 @@ defmodule WebAuthnLite.ClientDataJSON do
     try do
       with raw <- base64_url_encoded_client_data_json |> Base.url_decode64!(padding: false),
            json <- raw |> Jason.decode!() do
-        {:ok, %__MODULE__{
-          type: json["type"],
-          origin: json["origin"],
-          challenge: json["challenge"],
-          raw: raw
-        }}
+        {:ok,
+         %__MODULE__{
+           type: json["type"],
+           origin: json["origin"],
+           challenge: json["challenge"],
+           raw: raw
+         }}
       end
     rescue
       _ -> {:error, :invalid_format}
@@ -36,8 +37,17 @@ defmodule WebAuthnLite.ClientDataJSON do
   @doc """
   validate Base64 URL encoded clientDataJSON with params and return struct.
   """
-  @spec validate(base64_url_encoded_client_data_json :: String.t(), type :: String.t, origin :: String.t, challenge :: String.t) ::
-          {:ok, t} | {:error, :invalid_format} | {:error, :invalid_type} | {:error, :invalid_origin} | {:error, :invalid_challenge}
+  @spec validate(
+          base64_url_encoded_client_data_json :: String.t(),
+          type :: String.t(),
+          origin :: String.t(),
+          challenge :: String.t()
+        ) ::
+          {:ok, t}
+          | {:error, :invalid_format}
+          | {:error, :invalid_type}
+          | {:error, :invalid_origin}
+          | {:error, :invalid_challenge}
   def validate(base64_url_encoded_client_data_json, type, origin, challenge) do
     with {:ok, client_data} <- decode(base64_url_encoded_client_data_json) do
       cond do
