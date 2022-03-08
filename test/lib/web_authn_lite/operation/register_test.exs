e9bf185a073314063998f78a9c518569921e1f6f
Tashiro <g4160hc@gmail.com>
2022-03-08 15:53:18 +0900

    variable "storable_public_key" is unused

--- test/lib/web_authn_lite/operation/register_test.exs
+++ test/lib/web_authn_lite/operation/register_test.exs
@@ -22,7 +22,7 @@ defmodule WebAuthnLite.Operation.RegisterTest do
   end
 
   test "validate_attestation_object" do
-    assert {:ok, storable_public_key = %StorablePublicKey{}, _attestation_object} =
+    assert {:ok, _storable_public_key = %StorablePublicKey{}, _attestation_object} =
              Register.validate_attestation_object(%{
                attestation_object: @encoded_attestation_object,
                client_data_json: @encoded_client_data_json,
