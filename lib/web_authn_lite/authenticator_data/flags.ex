882d3198d19e89d3d90a96d09f3b141b9cf63d21
ritou <ritou.06@gmail.com>
2023-09-29 19:33:04 +0900

    support be, bs flag

--- lib/web_authn_lite/authenticator_data/flags.ex
+++ lib/web_authn_lite/authenticator_data/flags.ex
@@ -5,12 +5,14 @@ defmodule WebAuthnLite.AuthenticatorData.Flags do
   see https://www.w3.org/TR/webauthn/#flags
   """
 
-  defstruct [:flags, :up, :uv, :at, :ed]
+  defstruct [:flags, :up, :uv, :be, :bs, :at, :ed]
 
   @type t :: %__MODULE__{
           flags: binary,
           up: boolean,
           uv: boolean,
+          be: boolean,
+          bs: boolean,
           at: boolean,
           ed: boolean
         }
@@ -23,9 +25,20 @@ defmodule WebAuthnLite.AuthenticatorData.Flags do
   @spec from_binary(flags :: binary) :: {:ok, t} | {:error, :invalid_format}
   def from_binary(flags) do
     with true <- flags |> byte_size() == @flags_byte_size,
-         <<ed::size(1), at::size(1), _rfu2::size(3), uv::size(1), _rfu1::size(1), up::size(1)>> <-
+         <<ed::size(1), at::size(1), _rfu2::size(1), bs::size(1), be::size(1), uv::size(1),
+           _rfu1::size(1),
+           up::size(1)>> <-
            flags do
-      {:ok, %__MODULE__{flags: flags, up: up == 1, uv: uv == 1, at: at == 1, ed: ed == 1}}
+      {:ok,
+       %__MODULE__{
+         flags: flags,
+         up: up == 1,
+         uv: uv == 1,
+         be: be == 1,
+         bs: bs == 1,
+         at: at == 1,
+         ed: ed == 1
+       }}
     else
       _ -> {:error, :invalid_format}
     end
